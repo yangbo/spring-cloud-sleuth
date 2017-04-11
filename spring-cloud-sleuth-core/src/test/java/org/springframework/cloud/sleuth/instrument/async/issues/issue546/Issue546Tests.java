6865a625859947f531ae7e6e80b8891227f0ef51
Marcin Grzejszczak <marcin@grzejszczak.pl>
2017-04-11 09:47:06 +0200

    WIP

--- spring-cloud-sleuth-core/src/test/java/org/springframework/cloud/sleuth/instrument/async/issues/issue546/Issue546Tests.java
+++ spring-cloud-sleuth-core/src/test/java/org/springframework/cloud/sleuth/instrument/async/issues/issue546/Issue546Tests.java
@@ -17,7 +17,6 @@
 package org.springframework.cloud.sleuth.instrument.async.issues.issue546;
 
 import java.lang.invoke.MethodHandles;
-import java.util.concurrent.Executor;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -25,19 +24,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.sleuth.Sampler;
-import org.springframework.cloud.sleuth.Span;
 import org.springframework.cloud.sleuth.Tracer;
-import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -46,42 +38,25 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.AsyncRestTemplate;
 import org.springframework.web.client.RestTemplate;
 
-import com.jayway.awaitility.Awaitility;
-
 import static org.springframework.cloud.sleuth.assertions.SleuthAssertions.then;
 
 /**
  * @author Marcin Grzejszczak
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class,
+@SpringBootTest(classes = AsyncSleuthTestApplication.class,
 		properties = {"ribbon.eureka.enabled=false", "feign.hystrix.enabled=false", "server.port=0"},
 		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class Issue546Tests {
 
 	@Autowired Environment environment;
-	@Autowired Tracer tracer;
-	@Autowired AsyncTask asyncTask;
 	@Autowired RestTemplate restTemplate;
-	/**
-	 * Related to issue #445
-	 */
-	@Autowired Application.MyService executorService;
 
 	@Test
-	public void should_pass_tracing_info_for_tasks_running_without_a_pool() {
-		Span span = this.tracer.createSpan("foo");
-		try {
-			String response = this.restTemplate.getForObject("http://localhost:" + port() + "/without_pool", String.class);
-
-			then(response).isEqualTo(span.traceIdString());
-			Awaitility.await().until(() -> {
-				then(this.asyncTask.getSpan().get()).isNotNull();
-				then(this.asyncTask.getSpan().get().getTraceId()).isEqualTo(span.getTraceId());
-			});
-		} finally {
-			this.tracer.close(span);
-		}
+	public void should_pass_tracing_info_when_using_callbacks() {
+		this.restTemplate
+				.getForObject("http://localhost:" + port() + "/trace-async-rest-template",
+						String.class);
 	}
 
 	private int port() {
@@ -89,24 +64,13 @@ public class Issue546Tests {
 	}
 }
 
-@Configuration
-@EnableAsync
-class AppConfig {
+@SpringBootApplication
+class AsyncSleuthTestApplication {
 
-	@Bean public Sampler testSampler() {
-		return new AlwaysSampler();
-	}
-
-	@Bean public RestTemplate restTemplate() {
+	@Bean
+	RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
-
-	@Bean public Executor poolTaskExecutor() {
-		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-		executor.initialize();
-		return executor;
-	}
-
 }
 
 @RestController
@@ -114,17 +78,14 @@ class Controller {
 	private static final Log log = LogFactory.getLog(MethodHandles.lookup().lookupClass());
 
 	private final AsyncRestTemplate traceAsyncRestTemplate;
+	private final Tracer tracer;
 
-	private final Task task;
-
-	public Controller(AsyncRestTemplate traceAsyncRestTemplate, Task task) {
+	public Controller(AsyncRestTemplate traceAsyncRestTemplate, Tracer tracer) {
 		this.traceAsyncRestTemplate = traceAsyncRestTemplate;
-		this.task = task;
+		this.tracer = tracer;
 	}
 
 	@Value("${server.port}") private String port;
-
-	@Value("${app.activatesSleep:true}") private boolean activatesSleep;
 
 	@RequestMapping(value = "/bean") public HogeBean bean() {
 		log.info("(/bean) I got a request!");
@@ -135,6 +96,7 @@ class Controller {
 	public void asyncTest(@RequestParam(required = false) boolean isSleep)
 			throws InterruptedException {
 		log.info("(/trace-async-rest-template) I got a request!");
+		final long traceId = tracer.getCurrentSpan().getTraceId();
 		ListenableFuture<ResponseEntity<HogeBean>> res = traceAsyncRestTemplate
 				.getForEntity("http://localhost:" + port + "/bean", HogeBean.class);
 		if (isSleep) {
@@ -142,34 +104,13 @@ class Controller {
 		}
 		res.addCallback(success -> {
 			log.info("(/trace-async-rest-template) success");
+			then(Controller.this.tracer.getCurrentSpan()).hasTraceIdEqualTo(traceId);
 		}, failure -> {
 			log.error("(/trace-async-rest-template) failure", failure);
+			then(Controller.this.tracer.getCurrentSpan()).hasTraceIdEqualTo(traceId);
 		});
 	}
 
-	@RequestMapping(value = "/async-annotation") public void asyncTest2()
-			throws InterruptedException {
-		log.info("(/async-annotation) start");
-		task.doSomething();
-		log.info("(/async-annotation) end");
-	}
-}
-
-@Component
-class Task {
-
-	private static final Log log = LogFactory.getLog(MethodHandles.lookup().lookupClass());
-
-	@Async
-	void doSomething() {
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			log.error(e.getMessage(), e);
-		}
-
-		log.info("doSomething() done");
-	}
 }
 
 class HogeBean {
